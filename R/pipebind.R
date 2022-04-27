8c0b3fb8c3fec76a9ae8cf19dab0827df3caaf64
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2022-04-27 15:18:08 -0400

    Initial commit

---
+++ R/pipebind.R
@@ -0,0 +1,57 @@
+#' Bind a (piped) object to a symbol for complex function evaluation
+#'
+#' The base R \code{\link[base:pipeOp]{|>}} pipe lacks some advanced
+#' functionality compared to the *magrittr* \code{\link[magrittr:%>%]{%>%}}
+#' pipe. For example, the piped object can only appear once on the right-hand
+#' side of the pipe (either as the first argument or elsewhere using the \code{_}
+#' placeholder in R 4.2.0 and later), and the \code{_} placeholder cannot appear
+#' on the left side of sub-setting functions like `$`, `[`, `[[`, or `@`.
+#'
+#' The `bind()` function is a way to conveniently circumvent these limitations.
+#' Pipe an object into `bind()`, choose a placeholder symbol to represent it,
+#' then use this placeholder to refer the piped object in any way and as many
+#' times as desired in an R expression.
+#'
+#' @param .pipeValue The object to bind. Typically specified by pipe into the
+#'   `bind()` function (e.g., `x |> bind()`).
+#' @param .pipeBind The placeholder symbol to use to represent the piped object.
+#'   Can be any valid R object name.
+#' @param ... An R expression. Any valid R code (expression).
+#'
+#' @return The results of the expression, evaluated using the piped object.
+#'
+#' @export
+#'
+#' @examples
+#' if (getRversion() >= 4.1) {
+#'
+#'   # Piping to a non-first argument
+#'   mtcars |>
+#'     transform(kmL = mpg / 2.35) |>
+#'     bind(d, lm(kmL ~ hp, data = d))
+#'
+#'   # Using the piped value multiple times
+#'   rnorm(10, mean = 10) |>
+#'     bind(x, x - mean(x))
+#'
+#'   # Using the piped value in multiple arguments
+#'   c(a = 1, b = 2, c = 3) |>
+#'     bind(x, paste(names(x), x, sep = " = "))
+#'
+#'   # Subsetting the piped value
+#'   mtcars |>
+#'     bind(d, d$mpg)
+#' }
+bind <- function(.pipeValue, .pipeBind, ...) {
+  args <- alist(. = )
+  names(args) = as.character(substitute(.pipeBind))
+  .pipeFunction <- eval(
+    expr = call(
+      "function",
+      as.pairlist(args),
+      substitute(...)
+    ),
+    envir = parent.frame()
+  )
+  .pipeFunction(.pipeValue)
+}
---
+++ README.md
@@ -0,0 +1,67 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# pipebind: Bind a (piped) object to a symbol for complex function evaluation
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The base R `|>` pipe lacks some advanced functionality compared to the
+*magrittr* `%>%` pipe. For example, the piped object can only appear
+once on the right-hand side of the pipe (either as the first argument or
+elsewhere using the `_` placeholder), and the `_` placeholder cannot
+appear on the left side of sub-setting functions like `$`, `[`, `[[`, or
+`@`.
+
+This package provides a `bind()` function as a way to conveniently
+circumvent these limitations. Pipe an object into `bind()`, choose a
+placeholder symbol to represent it, then use this placeholder to refer
+the piped object in any way and as many times as desired in an R
+expression.
+
+## Installation
+
+You can install the development version of *pipebind* like so:
+
+``` r
+remotes::install_github("bwiernik/pipe)
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(pipebind)
+
+if (getRversion() >= 4.1) {
+  set.seed(2016)
+  
+  # Piping to a non-first argument
+  mtcars |>
+    transform(kmL = mpg / 2.35) |>
+    bind(d, lm(kmL ~ hp, data = d))
+  
+  # Using the piped value multiple times
+  rnorm(10) |>
+    bind(x, x - mean(x))
+  
+  # Using the piped value in multiple arguments
+  c(a = 1, b = 2, c = 3) |>
+    bind(x, paste(names(x), x, sep = " = "))
+  
+  # Subsetting the piped value
+  mtcars |>
+    bind(d, d$mpg)
+}
+#>  [1] 21.0 21.0 22.8 21.4 18.7 18.1 14.3 24.4 22.8 19.2 17.8 16.4 17.3 15.2 10.4
+#> [16] 10.4 14.7 32.4 30.4 33.9 21.5 15.5 15.2 13.3 19.2 27.3 26.0 30.4 15.8 19.7
+#> [31] 15.0 21.4
+```
+
+## Code of Conduct
+
+Please note that the pipebind project is released with a [Contributor
+Code of
+Conduct](https://contributor-covenant.org/version/2/0/CODE_OF_CONDUCT.html).
+By contributing to this project, you agree to abide by its terms.
