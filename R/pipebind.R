2ee15b18619e049ebc29c74e4478987d5361abe9
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2022-08-10 10:11:53 -0400

    update documentation

--- R/pipebind.R
+++ R/pipebind.R
@@ -1,12 +1,12 @@
 #' Bind a (piped) object to a symbol for complex function evaluation
 #'
 #' @description
-#' The base R \code{\link[base:pipeOp]{|>}} pipe lacks some advanced
-#' functionality compared to the
-#' \href{https://magrittr.tidyverse.org/reference/pipe.html}{\emph{magrittr} \code{%>%}}
-#' pipe. For example, the piped object can only appear once on the right-hand
+#' The \link[base:pipeOp]{base R} \code{\link[base:pipeOp]{|>}} \link[base:pipeOp]{pipe}
+#' lacks some advanced functionality compared to the
+#' \href{https://magrittr.tidyverse.org/reference/pipe.html}{`{magrittr}` `%>%` pipe}.
+#' For example, the piped object can only appear once on the right-hand
 #' side of the pipe (either as the first unnamed argument or elsewhere using the
-#' \code{_} placeholder in R 4.2.0 and later), and the \code{_} placeholder cannot
+#' `_` placeholder in R 4.2.0 and later), and the `_` placeholder cannot
 #' appear on the left side of sub-setting functions like `$`, `[`, `[[`, or `@`.
 #'
 #' The `bind()` function is a way to conveniently circumvent these limitations.
@@ -14,7 +14,13 @@
 #' then use this placeholder to refer the piped object in any way and as many
 #' times as desired in an R expression.
 #'
-#' @param .pipeValue The object to bind. Typically specified by pipe into the
+#' The Greek letter
+#' \ifelse{latex}{\out{$\lambda$}\code{()}}{\ifelse{html}{\out{&lambda;}\code{()}}{λ()}}
+#' is available as an alias for \code{bind()}.
+#'
+#' @aliases lambda pipebind
+#'
+#' @param .pipeValue The object to bind. Typically specified by piping into the
 #'   `bind()` function (e.g., `x |> bind()`).
 #' @param .pipeBind The placeholder symbol to use to represent the piped object.
 #'   Can be any valid R object name.
@@ -23,7 +29,6 @@
 #' @return The results of the expression, evaluated using the piped object.
 #'
 #' @export
-#' @aliases λ
 #'
 #' @examples
 #' if (getRversion() >= 4.1) {
@@ -61,6 +66,5 @@ bind <- function(.pipeValue, .pipeBind, ...) {
 
 #' @rdname bind
 #' @name bind
-#' @usage \if{html,text}{λ(.pipeValue, .pipeBind, ...)}
 #' @export
 assign("\u03bb", bind)
---
+++ cran-comments.md
@@ -0,0 +1,11 @@
+## R CMD check results
+
+## Test environments
+
+- R-hub windows-x86_64-devel (r-devel)
+- R-hub ubuntu-gcc-release (r-release)
+- R-hub fedora-clang-devel (r-devel)
+
+## R CMD check results
+
+0 errors ✔ | 0 warnings ✔ | 0 notes ✔
